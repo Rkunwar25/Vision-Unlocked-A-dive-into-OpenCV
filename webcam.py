0672a56366a5bbd758a84a7155fef4ec724d79d9
yashkk07 <ykarnani@gitam.in>
2025-01-28 23:51:06 +0530

    update-code

--- webcam.py
+++ webcam.py
@@ -7,9 +7,9 @@ cap = cv2.VideoCapture(0) #for webcam
 cap.set(3, 640)
 cap.set(4, 480)
 
-#cap = cv2.VideoCapture("Videos/people.mp4") #for video
+# cap = cv2.VideoCapture("Videos/motorbikes.mp4") #for video
 
-model = YOLO("yolo weights/yolov8l.pt")
+model = YOLO("yolov8l.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat",
               "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
@@ -31,8 +31,6 @@ while True:
             #bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            #print(x1, y1, x2, y2)
-            #cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)
 
             w, h = x2-x1, y2-y1
             cvzone.cornerRect(img, (x1, y1, w, h))
@@ -40,13 +38,10 @@ while True:
             #confidence
             conf = math.ceil((box.conf[0]*100))/100
             print(conf)
-            #cvzone.putTextRect(img, f'{conf}', (max(0, x1), max(40, y1)))
 
             #classname
             cls = box.cls[0]
             cvzone.putTextRect(img, f'{classNames[int(cls)]} {conf}', (max(0, x1), max(40, y1)), scale=1, thickness=1)
 
-
     cv2.imshow("Image", img)
     cv2.waitKey(1)
-
